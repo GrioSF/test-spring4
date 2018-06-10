0da41550b4060b1f2bc9bc6338bc4e059bd706f7
Eric Hamlin <ehamlin@grio.com>
2018-06-10 18:43:32 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/grio/testspring/TestSpring4Application.java
@@ -0,0 +1,12 @@
+package com.grio.testspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpring4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpring4Application.class, args);
+	}
+}
